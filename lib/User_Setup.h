1e2390deebfaf316e278a71c7980243a77eacd8c
David <atunwadavidb@gmail.com>
2026-07-03 11:34:31 +0100

    added User_Setup.h configs

---
+++ lib/User_Setup.h
@@ -0,0 +1,24 @@
+#define USER_SETUP_LOADED
+
+#define ILI9341_DRIVER
+#define TFT_RGB_ORDER TFT_BGR
+
+#define TFT_CS 5
+#define TFT_DC 2
+#define TFT_RST 4
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_MISO 19
+#define TFT_BL -1
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+
+#define SMOOTH_FONT
+
+#define SPI_FREQUENCY 20000000
